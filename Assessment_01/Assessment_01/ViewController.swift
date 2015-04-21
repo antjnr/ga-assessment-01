6f7940217cefe6ed1fed4e0cc58cb6c9f32f3498
Anthony Junior <anthony.coelho@me.com>
2015-04-21 18:39:51 +1000

    Initial commit of project files

---
+++ Assessment_01/Assessment_01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assessment_01
+//
+//  Created by Anthony Coelho on 21/04/2015.
+//  Copyright (c) 2015 Anthony Coelho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
